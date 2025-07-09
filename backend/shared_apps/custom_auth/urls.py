a112ab16bab7a182abf99fcae8363ce3aa8a425e
roshanjabir <roshanjabir7@gmail.com>
2025-07-09 13:40:53 +0530

    feat(backend): Added swagger ui and redoc to be available in tenant urls and shared urls

--- backend/shared_apps/custom_auth/urls.py
+++ backend/shared_apps/custom_auth/urls.py
@@ -3,6 +3,22 @@ from shared_apps.custom_auth.views import (
     MyTokenObtainPairView, MyTokenRefreshView,
     LogoutView, MeView, index
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="API for all endpoints. \n\n"
+            "➡️ Use `/token/` to log in.\n"
+            "➡️ In production, JWTs are stored in HttpOnly cookies.\n"
+            "➡️ For Swagger testing, paste your token above as: `Bearer <access_token>`."
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', index, name='client_index'),
@@ -10,4 +26,7 @@ urlpatterns = [
     path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', MeView.as_view(), name='me'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- backend/shared_apps/tenants/urls.py
+++ backend/shared_apps/tenants/urls.py
@@ -3,19 +3,6 @@ from shared_apps.tenants import views
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="My API",
-      default_version='v1',
-      description="API for all endpoints",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path("api/", include([
@@ -27,8 +14,5 @@ urlpatterns = [
         path("tenants/send-otp/", views.SendOTPView.as_view(), name="send-otp"),
         path("tenants/verify-otp/", views.VerifyOTPView.as_view(), name="verify-otp"),
 
-
-        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     ])),
 ]
