2408a12c2b01896487d5da6eb8427016692af341
roshanjabir <roshanjabir7@gmail.com>
2025-07-09 20:32:49 +0530

    fix(backend): Fixed redis port issue

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = SHARED_APPS + [app for app in TENANT_APPS if app not in SHARED_
 ASGI_APPLICATION = "backend.asgi.application"
 
 # Celery Settings
-CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
