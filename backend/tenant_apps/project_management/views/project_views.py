4c7dc068e7d5b41fd8e462067d50e129e37d1db8
roshanjabir <roshanjabir7@gmail.com>
2025-07-13 21:34:19 +0530

    feat(backend): Cleaned unused imports of files in project_management/views

--- backend/tenant_apps/project_management/views/project_views.py
+++ backend/tenant_apps/project_management/views/project_views.py
@@ -1,48 +1,12 @@
-from rest_framework import viewsets, generics, status, filters, permissions
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.response import Response
-from collections import defaultdict
-from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.exceptions import PermissionDenied
-from django_filters.rest_framework import DjangoFilterBackend
-import django_filters
-from django.contrib.contenttypes.models import ContentType
-import logging
-logger = logging.getLogger(__name__)
-from django.shortcuts import get_object_or_404
-from tenant_apps.employee.models import Employee, ProjectManagerAssignment, Notification
-from tenant_apps.project_management.models import Project, ProjectMember, Task, Subtask, DeveloperAssignmentAuditLog, SubtaskAssignmentAudit, Label, Comment
-from tenant_apps.project_management.tasks.email_tasks import send_pm_blocking_subtasks_email
-from django.core.exceptions import ObjectDoesNotExist
-
-
-from core.constants import UserRoles, TaskStatus
-from core.guards.project_guards import (
-    ensure_project_is_active,
-    ensure_active_via_member,
-    ensure_active_via_task,
-    ensure_active_via_subtask
-)
-from core.permissions import (
-    IsTenantAdmin,
-    IsProjectManagerOrTenantAdmin,
-    IsProjectReadOnlyOrManager,
-    IsAssigneeOrManager
-)
 from rest_framework.permissions import IsAuthenticated
-from .serializers import (
-    ProjectSerializer,
-    ProjectMemberSerializer,
-    TaskSerializer,
-    SubtaskSerializer,
-    ProjectManagerAssignmentSerializer,
-    SimpleEmployeeSerializer,
-    DeveloperAssignmentAuditLogSerializer,
-    LabelSerializer,
-    CommentSerializer,
-    SubtaskAssignmentAuditSerializer,
-)
+from rest_framework.response import Response
+
+from tenant_apps.project_management.models import Project, Task
+from core.permissions import IsProjectReadOnlyOrManager
+from .serializers import ProjectSerializer, TaskSerializer
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
